a77eea6820d236cddbb4a70d986ca84fefff0e34
OmniOx750 <mek03@mekics.com>
2026-08-05 14:32:47 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 window.MEAL_APP_CONFIG = Object.freeze({
-  GAS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbxOEQ8WTehbl-cOrZlkLx_FPbgZWkGBHyjEt1msdysuNR4j9g6XLH4hno2dD7YRS_xU/exec',
+  GAS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbwarc4zgwQ-5M1g4LUXNSiA6nzh9sIzLYedrQ2UatZURECRw2YjPIWpbZ8Wbjs3nNjb/exec',
   APP_TITLE: '사내 식수 신청',
   COMPANY_NAME: '멕아이씨에스',
   API_TIMEOUT_MS: 30000
